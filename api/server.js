15c7c4546a0a359f4c0bc256572bb8f8589ed2ba
Lia Neville <lianeville5@gmail.com>
2023-09-30 11:28:28 -0400

    pulls message from mongo

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb")
+const { MongoClient, ObjectId } = require("mongodb")
 const uri =
 	"mongodb+srv://lianeville5:u9xKoDoJ0fuPqm89@chat-app.kczrold.mongodb.net/?retryWrites=true&w=majority"
 
@@ -8,6 +8,28 @@ async function connectToMongo() {
 	try {
 		await client.connect()
 		console.log("Connected to MongoDB")
+
+		const db = client.db("Chat-App")
+		const sourceCollection = db.collection("Messages")
+		const sessionId = new ObjectId("65182b51e8c3908380a5c36b")
+
+		let sessionMessages = sourceCollection.find({
+			session_id: sessionId,
+		})
+
+		sessionMessages
+			.toArray()
+			.then(messages => {
+				// Iterate through the messages and log each one
+				messages.forEach(message => {
+					console.log(message)
+				})
+				client.close()
+			})
+			.catch(error => {
+				console.error("An error occurred:", error)
+				client.close()
+			})
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error)
 	}
