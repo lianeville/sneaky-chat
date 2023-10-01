e98b9cbc705e810b4a800eb347666136634ee608
Lia Neville <lianeville5@gmail.com>
2023-10-01 10:55:58 -0400

    gets data from server to frontend

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
+const express = require("express")
+const cors = require("cors") // Import the cors middleware
+
+const app = express()
+const port = 8000
+
 const { MongoClient, ObjectId } = require("mongodb")
 const uri =
 	"mongodb+srv://lianeville5:u9xKoDoJ0fuPqm89@chat-app.kczrold.mongodb.net/?retryWrites=true&w=majority"
 
 const client = new MongoClient(uri)
+
+app.use(cors())
 
 async function connectToMongo() {
 	try {
@@ -17,22 +25,18 @@ async function connectToMongo() {
 			session_id: sessionId,
 		})
 
-		sessionMessages
-			.toArray()
-			.then(messages => {
-				// Iterate through the messages and log each one
-				messages.forEach(message => {
-					console.log(message)
-				})
-				client.close()
-			})
-			.catch(error => {
-				console.error("An error occurred:", error)
-				client.close()
-			})
+		let messages = await sessionMessages.toArray()
+		return messages
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error)
 	}
 }
 
-connectToMongo()
+app.get("/", async (req, res) => {
+	const messages = await connectToMongo()
+	res.json(messages)
+})
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
--- app/src/components/Chat/MessageFeed.jsx
+++ app/src/components/Chat/MessageFeed.jsx
@@ -3,6 +3,24 @@ import MessageContainer from "./MessageContainer"
 import SendMessageContainer from "./SendMessageContainer"
 
 class MessageFeed extends Component {
+	componentDidMount() {
+		console.log("hiiii")
+		fetch("http://localhost:8000/")
+			.then(response => {
+				if (!response.ok) {
+					throw new Error("Network response was not ok")
+				}
+				return response.json()
+			})
+			.then(data => {
+				// Handle the data here (e.g., update the component state)
+				console.log(data)
+			})
+			.catch(error => {
+				console.error("Error fetching data:", error)
+			})
+	}
+
 	render() {
 		return (
 			<div className="w-full absolute inset-0 flex flex-col-reverse">
